5cab9bfa9426c73b2c732cbc0c2decb8caec792d
Max Liles <lileswm@miamioh.edu>
2021-06-23 20:00:48 -0400

    earthquake data and graphs

--- sitka_highs.py
+++ sitka_highs.py
@@ -12,14 +12,20 @@ with open(filename) as f:
         print(index, column_header)
 
     # Get dates and high and low temps from this file.
-    dates, highs, lows = [], [], []
+    dates, highs, lows, = [], [], [], 
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[5])
-        low = int(row[6])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[5])
+            low = int(row[6])
+           
+        except ValueError:
+            print(f'Missing data for {current_date}')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+            
 
 # Plot the high and low temps
 plt.style.use('seaborn')
@@ -27,6 +33,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red', alpha=0.5)
 ax.plot(dates, lows, c='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
 
 # Format plot
 ax.set_title('Daily high and low temperatures - 2018', fontsize=24)
